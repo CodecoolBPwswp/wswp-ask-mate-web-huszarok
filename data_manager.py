d0576f8afaf1b956174619acb0af66315f3d52d7
kinga00099 <kinga00099@gmail.com>
2018-07-03 13:42:03 +0200

    Sort questions by submission date (descending order)

--- data_manager.py
+++ data_manager.py
@@ -3,12 +3,10 @@ Functions here should be called from the server.py and these should use generic 
 
 import connection
 import datetime
-
+from operator import itemgetter
 
 def get_questions_from_file():
     list_of_questions = connection.get_data_from_file('sample_data/question.csv')
-    for question in list_of_questions:
-        question['submission_time'] = convert_submission_time_to_date(question['submission_time'])
     return list_of_questions
 
 
@@ -20,4 +18,12 @@ def get_answers_from_file():
 def convert_submission_time_to_date(timestamp):
     time = datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
     return time
+
+
+def sort_questions_by_date():
+    list_of_questions = get_questions_from_file()
+    list_of_questions = sorted(list_of_questions, key=itemgetter('submission_time'), reverse=True)
+    for question in list_of_questions:
+        question['submission_time'] = convert_submission_time_to_date(question['submission_time'])
+    return list_of_questions
 
