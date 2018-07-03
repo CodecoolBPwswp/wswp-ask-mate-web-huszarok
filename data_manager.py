d598d6ad395aea31154e46acd3b427ba1991e52c
kinga00099 <kinga00099@gmail.com>
2018-07-03 13:12:39 +0200

    Unix timestamp conversion to readable date

--- data_manager.py
+++ data_manager.py
@@ -2,13 +2,22 @@
 Functions here should be called from the server.py and these should use generic functions from the connection.py'''
 
 import connection
+import datetime
+
 
 def get_questions_from_file():
     list_of_questions = connection.get_data_from_file('sample_data/question.csv')
+    for question in list_of_questions:
+        question['submission_time'] = convert_submission_time_to_date(question['submission_time'])
     return list_of_questions
 
 
 def get_answers_from_file():
     list_of_answers = connection.get_data_from_file('answer.csv')
     return list_of_answers
+
+
+def convert_submission_time_to_date(timestamp):
+    time = datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+    return time
 
