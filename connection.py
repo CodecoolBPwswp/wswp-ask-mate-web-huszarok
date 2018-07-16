9934d08ed5fc999ff1c964a6203f7b78fa5a704b
kinga00099 <kinga00099@gmail.com>
2018-07-16 14:18:07 +0200

    database created

--- connection.py
+++ connection.py
@@ -1,42 +1,48 @@
-'''Common functions to read/write/append CSV files without feature specific knowledge.
-The layer that have access to any kind of long term data storage.
-In this case, we use CSV files, but later on we'll change this to SQL database. So in the future, we only need to change in this layer.'''
+# Creates a decorator to handle the database connection/cursor opening/closing.
+# Creates the cursor with RealDictCursor, thus it returns real dictionaries, where the column names are the keys.
+import os
+import psycopg2
+import psycopg2.extras
 
-import csv
+def get_connection_string():
+    # setup connection string
+    # to do this, please define these environment variables first
+    user_name = os.environ.get('PSQL_USER_NAME')
+    password = os.environ.get('PSQL_PASSWORD')
+    host = os.environ.get('PSQL_HOST')
+    database_name = os.environ.get('PSQL_DB_NAME')
 
+    env_variables_defined = user_name and password and host and database_name
 
-def get_data_from_file(data):
-    list_of_entries = []
-    filename = "%s" % data
-    with open(filename, "r") as file:
-        entries = csv.DictReader(file)
-        for line in entries:
-            list_of_entries.append(line)
-    return list_of_entries
+    if env_variables_defined:
+        # this string describes all info for psycopg2 to connect to the database
+        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
+            user_name=user_name,
+            password=password,
+            host=host,
+            database_name=database_name
+        )
+    else:
+        raise KeyError('Some necessary environment variable(s) are not defined')
 
-
-def append_data_to_file(data, question_data):
-    filename = "%s" % data
-    with open(filename, "a") as file:
-        writer = csv.writer(file)
-        writer.writerow(question_data)
-
-
-def update_data_in_file(data, updated_question_data):
-    list_of_entries = []
-    filename = "%s" % data
-    with open(filename, "r") as file:
-        entries = csv.DictReader(file)
-        for line in entries:
-            if line['id'] == updated_question_data['id']:
-                list_of_entries.append(updated_question_data)
-            else:
-                list_of_entries.append(line)
-    with open(filename, "w") as file:
-        fieldnames = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(list_of_entries)
+def open_database():
+    try:
+        connection_string = get_connection_string()
+        connection = psycopg2.connect(connection_string)
+        connection.autocommit = True
+    except psycopg2.DatabaseError as exception:
+        print('Database connection problem')
+        raise exception
+    return connection
 
 
-
+def connection_handler(function):
+    def wrapper(*args, **kwargs):
+        connection = open_database()
+        # we set the cursor_factory parameter to return with a RealDictCursor cursor (cursor which provide dictionaries)
+        dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        ret_value = function(dict_cur, *args, **kwargs)
+        dict_cur.close()
+        connection.close()
+        return ret_value
+    return wrapper
