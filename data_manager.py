eb3dff42bf9fa9c061c1f8e0a8368ff08d67461c
kinga00099 <kinga00099@gmail.com>
2018-07-16 16:51:28 +0200

    common function instead of different get data functions for each table

--- data_manager.py
+++ data_manager.py
@@ -6,19 +6,28 @@ import datetime
 from operator import itemgetter
 import util
 import time
+from psycopg2 import sql
+
+
 
 @connection.connection_handler
-def get_questions_from_file(cursor):
-    '''def get_questions_from_file():
-        list_of_questions = connection.get_data_from_file('sample_data/question.csv')
-        return list_of_questions'''
-    cursor.execute("""
-                    SELECT * FROM question
-                    ORDER BY submission_time DESC;
-                   """)
-    list_of_questions = cursor.fetchall()
-    return list_of_questions
-
+def get_data_from_file(cursor, columns, table, order_by):
+    '''Use this function to access any columns of any table, ordered by any column in descending order
+    give the parameters to this function in server.py
+    columns: list of strings, strings are the chosen columns example: ['vote_number', 'title', 'message']
+    table: table name as string  example: 'question'
+    order_by: column name as string  example: 'submission_time'
+    '''
+    used_columns = sql.SQL(', ').join(sql.Identifier(n) for n in columns)
+    cursor.execute(
+        sql.SQL("""select {col} from {table} 
+                ORDER BY {order_column} DESC """)
+            .format(col= used_columns,
+                   table=sql.Identifier(table),
+                   order_column=sql.Identifier(order_by))
+    )
+    list_of_data = cursor.fetchall()
+    return list_of_data
 
 
 
