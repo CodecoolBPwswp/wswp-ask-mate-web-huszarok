9af0f7f1bff03d2d586a117192b8f202b8059f26
kinga00099 <kinga00099@gmail.com>
2018-07-30 16:43:40 +0200

    login HTML page prepared

--- server.py
+++ server.py
@@ -259,6 +259,13 @@ def register():
         return redirect('/')
 
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        return redirect('/')
+
 
 
 if __name__ == '__main__':
