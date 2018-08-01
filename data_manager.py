2a143c2cd8d5a2761de80e8cb470852f0667c46c
Viktor Zsolnai <zsviktor45@gmail.com>
2018-08-01 13:22:54 +0200

    idk

--- data_manager.py
+++ data_manager.py
@@ -250,9 +250,9 @@ def get_all_user_data(cursor):
     cursor.execute("""
                     SELECT users.username, users.email, COUNT(question.userid) as question, COUNT(answer.userid) as answer, COUNT(comment.userid) as comment
                     FROM users
-                    JOIN question ON question.userid = users.id
-                    JOIN answer ON answer.userid = users.id
-                    JOIN comment ON comment.userid=users.id
+                    LEFT JOIN question ON question.userid = users.id
+                    LEFT JOIN answer ON answer.userid = users.id
+                    LEFT JOIN comment ON comment.userid=users.id
                     GROUP BY users.id, question.userid, answer.userid, comment.userid""")
 
     data = cursor.fetchall()
